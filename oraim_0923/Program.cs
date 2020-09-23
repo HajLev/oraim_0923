b5c5ad606b910ca0cce64ca1bec52d4bcd9b320a
Levente Hajdu <hajdu.levente@verebelyszki.hu>
2020-09-23 11:24:33 +0200

    wow

--- oraim_0923/Program.cs
+++ oraim_0923/Program.cs
@@ -10,7 +10,59 @@ namespace oraim_0923
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("sor");
+            bool x = true;
+            bool y = false;
+
+            /* ==  --akkor igaz, ha mindkettö igaz vagy mindkettő hamis (ha egyformák az operandusok)
+             * !=  --(kizáró vagy) akkor igaz, ha az egyik igaz a másik hamis (ha különbözőek az operandusok)
+             * ||  --akkor igaz, ha legalláb az egyik igaz  
+             * &&  --akkor igaz, ha mindkettő igaz */
+
+            int[] t = new int[10];
+
+            if ((t.Length == 7) == (DateTime.Now.DayOfWeek == DayOfWeek.Wednesday))
+            {
+                Console.WriteLine("Belefér.");
+            }
+            else
+            {
+                Console.WriteLine("Faszsag.");
+            }
+
+            int z = 10;
+            switch (z)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("ez 1");
+                        Console.WriteLine("további");
+                    }
+                    break;
+                case 2:
+                    {
+                        Console.WriteLine("Ez 2");
+                    }
+                    break;
+                case 5:
+                    {
+                        Console.WriteLine("Ez 5");
+                    }
+                    break;
+                case 3:
+                    {
+                        Console.WriteLine("három");
+                    }
+                    break;
+                case 10:
+                    {
+                        Console.WriteLine("Tíz");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Egyik se");
+                    break;
+            }
+
             Console.ReadKey();
         }
     }
