d7934c99c13382802fcc0bc6b55df95c513edafd
Hajdu Levente <hajdu.levente@vereb.dc>
2020-09-23 10:12:57 +0200

    Add project files.

---
+++ oraim_0923/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oraim_0923
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
